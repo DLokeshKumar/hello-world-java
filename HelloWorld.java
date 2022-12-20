42ea4f9ad9ffc8c09033243c73ba300d4dbc4a6b
lokesh <lokeshd.devops@gmail.com>
2022-12-20 08:31:46 +0000

    2nd time testing poll sum in jenkins

--- HelloWorld.java
+++ HelloWorld.java
@@ -38,6 +38,6 @@ public class HelloWorld {
     System.out.println("Hello, World");
     System.out.println("Hello, World .... Devops ......;;;;;;;;;;  ");
  System.out.println("Hello, World .... Devops .... for ;;;;;; .POLL SCM TESTING........;;;;;;;;;;  ");
-   
+    System.out.println("Hello, World .... Devops .... for ;;;;;; .POLL SCM  2nd TESTING........;;;;;;;;;;  ");
   }
 }
